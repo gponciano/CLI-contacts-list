0d52844874c62793a0d2df78eaa487035bb19714
gponciano <gab_bash@hotmail.com>
2025-09-22 07:47:54 +1000

    Adding update contact function

--- app.py
+++ app.py
@@ -12,6 +12,27 @@ def check_contacts(contacts_list):
         contact_phone = contact["phone"]
         print(f"{index}. Name: {contact_name},  Email: {contact_email},  Phone: {contact_phone},  Status: {favourite}")
     return
+def wait_for_enter():
+    input()
+
+def update_contact(contacts_list, contact_index, contact_new_name, contact_new_email, contact_new_phone):
+    contacts_index_adjust = int(contact_index) -1
+    if contacts_index_adjust >= 0 and contacts_index_adjust < len(contacts_list):
+        contacts_list[contacts_index_adjust]["name"] = contact_new_name
+        contacts_list[contacts_index_adjust]["email"] = contact_new_email
+        contacts_list[contacts_index_adjust]["phone"] = contact_new_phone
+        if contact_new_name != contact_name:
+            print(f"Contact {contact_index} renamed to {contact_new_name}")
+            if contact_new_email != contact_email:
+                print(f"Contact {contact_index} new email set to {contact_new_email}")
+                if contact_new_phone != contact_phone:
+                    print(f"Contact {contact_index} new phone set to {contact_new_phone}")
+    else:
+        print("\nContact chosen does not exist")
+    return
+
+
+
 
 contacts_list = []
 
@@ -34,6 +55,13 @@ while True:
         adding_contact(contacts_list, contact_name, contact_email, contact_phone)
     elif user_input == "2":
         check_contacts(contacts_list)
-    
+    elif user_input == "3":
+        check_contacts(contacts_list)
+        contact_index = input("Select the number of the contact you'd like to update: ")
+        print("\n Hit enter if you wish the attribute remains the same")
+        contact_new_name = input("\nPlease rename your contact: ")
+        contact_new_phone = input("\nPlease provide new phone number: ")
+        contact_new_email = input("\nPlease provide new email: ")
+        update_contact(contacts_list, contact_index, contact_new_name, contact_new_email, contact_new_phone)
     elif user_input == "7":
         break
